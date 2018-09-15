767b43deb3527331077369d28316aa3d86c75e5c
Julian <joticajulian@gmail.com>
2018-09-14 23:31:56 -0500

    footer and description for no pets

--- js/steempet.js
+++ js/steempet.js
@@ -2,11 +2,13 @@
 const app = new Vue({
     el: '#app-steempet',
     data: {
-      account: {name:''},
-      validAccount: false,      
+      account: {
+        name: ''
+      },
+      validAccount: false,
       validKey: false,
       hasPet: false,
-      hasKey: false,      
+      hasKey: false,
       editing: false,
       newPet: false,
       pet: EMPTY_PET,
@@ -18,16 +20,16 @@ const app = new Vue({
       this.getUser();
     },
     methods: {
-      toggleEdit: function(){
-        this.editing = !this.editing;        
-      },    
-      toggleNew: function(){
+      toggleEdit: function () {
+        this.editing = !this.editing;
+      },
+      toggleNew: function () {
         this.newPet = !this.newPet;
       },
       getUser: function () {
         query = getQuery();
         this.validAccount = false;
-        
+
         if (query.p) {
           steem.api.getAccounts([query.p], function (err, result) {
             if (err || !result || result.length == 0) {
@@ -38,10 +40,10 @@ const app = new Vue({
               app.title = "ya respondieron de steem";
               app.tit = true;
               var json_metadata = (app.account.json_metadata && app.account.json_metadata != '') ? JSON.parse(app.account.json_metadata) : {};
-              
+
               if (json_metadata.pets && json_metadata.pets.length > 0) {
                 app.hasPet = true;
-                console.log('@'+app.account.name + " has pets");
+                console.log('@' + app.account.name + " has pets");
                 if (query.key) {
                   app.hasKey = true;
                   try {
@@ -55,20 +57,23 @@ const app = new Vue({
                       json_metadata.pets[0].private.notes = CryptoJS.AES.decrypt(json_metadata.pets[0].private.notes, query.key).toString(CryptoJS.enc.Utf8);
                     app.validKey = true;
                     console.log("Successfull decrypt");
-                  } catch(e) {
+                  } catch (e) {
                     console.log("Incorrect key to decrypt the private data");
                     app.validKey = false;
                   }
-                }else{
+                } else {
                   console.log("There is no key to decrypt the private data");
                   app.hasKey = false;
                 }
                 app.pet = json_metadata.pets[0];
                 app.editing = false;
               } else {
-                console.log('@'+app.account.name + " does not have pets");
+                console.log('@' + app.account.name + " does not have pets");
                 app.hasPet = false;
-                app.pet = {public:{}, private:{}};
+                app.pet = {
+                  public: {},
+                  private: {}
+                };
               }
             }
           });
@@ -76,5 +81,39 @@ const app = new Vue({
           console.log("There is no query");
         }
       }
-    }
+    },
+    template: `
+      <div>
+      <pet-navbar></pet-navbar>
+      <pet-userinfo :name="this.account.name" :valid="this.validAccount"></pet-userinfo>
+      <div class="container">
+        <div v-if="hasPet">
+          <div class="right">
+            <button @click="toggleEdit" class="icon"><img src="images/round-create-24px.svg"></button>
+          </div>
+          <div v-if="editing">
+            <pet-create :pet_init="this.pet"></pet-create>
+          </div>
+          <div v-else>
+            <pet-arraydata :pet="this.pet"></pet-arraydata>                
+            <pet-qrcode :haspet="this.hasPet" :haskey="this.hasKey" :validkey="this.validKey"></pet-qrcode>
+          </div>
+        </div>
+        <div v-else>
+          <div class="center">
+            <div class="text-huge">Oops!</div>
+            <div class="text-big">@{{this.account.name}} does not have pets</div>
+            <div>Click below to add one</div>
+          </div>
+          <div class="center">
+            <button @click="toggleNew" class="icon"><img src="images/round-add-24px.svg"></button>
+          </div>
+          <div v-if="newPet">
+            <pet-create :pet_init="this.pet"></pet-create>
+          </div>
+        </div>
+      </div>
+      <footer>SteemPet by <a href="https://steemit.com/@jga">@jga</a></footer>
+      </div>
+    `,
   });
